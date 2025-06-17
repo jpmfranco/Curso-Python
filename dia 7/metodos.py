31fe409449c6b9ed803622d55c88b2d705799a85
Juan Pablo Mayagoitia Franco <103715117+jpmfranco@users.noreply.github.com>
2025-06-17 16:35:27 -0600

    dia 7

---
+++ dia 7/metodos.py
@@ -0,0 +1,19 @@
+class Pajaro:
+
+    alas = True
+
+    def __init__(self, color, especie):
+        self.color = color
+        self.especie = especie
+    
+    def piar(self):
+        print("pio, mi color es {}".format(self.color))
+
+    def volar(self,metros):
+        print(f"el pajaro ha volado {metros} metros")
+
+
+piolin = Pajaro("amarillo",'canario')
+
+piolin.piar()
+piolin.volar(50)
