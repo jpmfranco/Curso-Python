69cac4ebf7fd783b254dc0aff4cd634fadf7e4cd
Juan Pablo Mayagoitia Franco <103715117+jpmfranco@users.noreply.github.com>
2025-06-17 16:27:47 -0600

    dia 7

---
+++ dia 7/atributos.py
@@ -0,0 +1,15 @@
+class Pajaro:
+    
+    alas = True
+
+    def __init__(self, color, especie):
+        self.color = color
+        self.especie = especie
+
+mi_pajaro = Pajaro('negro','Tucan')
+
+print(f"mi pajaro es un {mi_pajaro.especie} y es de color {mi_pajaro.color}")
+
+print(Pajaro.alas)
+print(mi_pajaro.alas)
+
