bf801c64d7fa5ea1fc9110699f4ae05b3d96eee5
Juan Pablo Mayagoitia Franco <103715117+jpmfranco@users.noreply.github.com>
2025-06-19 14:19:06 -0600

    dia 9

---
+++ dia 9/os_and_shutil.py
@@ -0,0 +1,10 @@
+import os
+import send2trash
+# import shutil
+# print(os.getcwd())
+# archivo = open('curso.txt','w')
+# archivo.write('Texto de prueba')
+# archivo.close()
+# print(os.listdir())
+# shutil.move('curso.txt','C:\\Users\\pablo\\Escritorio')
+send2trash.send2trash("curso.txt")
