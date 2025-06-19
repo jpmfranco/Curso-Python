3ca75c14c1c60868ed67c1c0d7898cbbe62870ea
Juan Pablo Mayagoitia Franco <103715117+jpmfranco@users.noreply.github.com>
2025-06-19 14:31:42 -0600

    dia 9

--- dia 9/os_and_shutil.py
+++ dia 9/os_and_shutil.py
@@ -1,5 +1,5 @@
 import os
-import send2trash
+# import send2trash
 # import shutil
 # print(os.getcwd())
 # archivo = open('curso.txt','w')
@@ -7,4 +7,18 @@ import send2trash
 # archivo.close()
 # print(os.listdir())
 # shutil.move('curso.txt','C:\\Users\\pablo\\Escritorio')
-send2trash.send2trash("curso.txt")
+# print(os.walk("C:\\Users\\pablo\\Recetas"))
+
+
+ruta = "C:\\Users\\pablo\\Recetas"
+
+for carpeta,subcarpeta,archivo in os.walk(ruta):
+    print(f'En la carpeta {carpeta}')
+    print(f"las subcarpetas son: ")
+    for sub in subcarpeta:
+        print(f"\t{sub}")
+    print("los archivos son:")
+    for arc in archivo:
+        if arc.startswith("2015"):
+            print(f"\t{arc}")
+    print("\n")
