0c530c5106d837e892f965e11b8eb9634f2107d0
Juan Pablo Mayagoitia Franco <103715117+jpmfranco@users.noreply.github.com>
2025-06-23 13:57:04 -0600

    dia 10

--- dia 10/main.py
+++ dia 10/main.py
@@ -9,7 +9,8 @@ pantalla = pygame.display.set_mode((800,600))
 
 #configurar titulo e icono
 pygame.display.set_caption('Invasión Espacial')
-
+icono = pygame.image.load('ovni.png')
+pygame.display.set_icon(icono)
 #Loop del juego
 se_ejecuta  = True
 while se_ejecuta:
