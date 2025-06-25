47d5f6e1239ba3a4aed35d55c9e3e936e54a532b
Juan Pablo Mayagoitia Franco <103715117+jpmfranco@users.noreply.github.com>
2025-06-25 13:34:39 -0600

    dia 11

---
+++ dia 11/web_scraping.py
@@ -0,0 +1,13 @@
+import bs4
+import requests
+
+resultado = requests.get("https://escueladirecta-blog.blogspot.com/2024/07/buscas-trabajo-y-no-has-certificado-en.html")
+
+sopa = bs4.BeautifulSoup(resultado.text, 'lxml')
+
+# print(sopa.select("title")[0].get_text())
+# parrafo_esp  = sopa.select("p")[0].get_text()
+# columna_lateral = sopa.select(".r-snippetized")
+
+# for p in columna_lateral:
+#     print(p.get_text())
