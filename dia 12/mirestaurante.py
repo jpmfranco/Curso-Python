299c3bdd812f742943daf68c24f8121f3d38b6df
Juan Pablo Mayagoitia Franco <103715117+jpmfranco@users.noreply.github.com>
2025-06-26 15:09:07 -0600

    day 12

--- dia 12/mirestaurante.py
+++ dia 12/mirestaurante.py
@@ -6,7 +6,7 @@ app = Tk()
 
 
 # size of window
-app.geometry('1020x630+0+0')
+app.geometry('1220x630+0+0')
 
 #avoid max
 app.resizable(0,0)
@@ -18,6 +18,54 @@ app.title("My restaurant - Facturation system")
 app.config(bg='burlywood')
 
 # Avoid that the screen close
+
+#superior panel
+pansup = Frame(app, bd=1, relief=FLAT)
+pansup.pack(side=TOP)
+
+#title 
+titleeti = Label(pansup, text='Facturation System',fg='azure4',
+                 font=('Dosis',58),bg='burlywood',width=27)
+titleeti.grid(row=0,column=0)
+
+# Panel izquierdo
+panizq = Frame(app,bd=1,relief=FLAT)
+panizq.pack(side=LEFT)
+
+# Costs panel
+pancost = Frame(panizq,bd=1,relief=FLAT)
+pancost.pack(side=BOTTOM)
+
+# Food panel
+pancom = LabelFrame(panizq,text='Food',font=('Dosis',19,'bold'),
+                    bd=1, relief=FLAT, fg='azure4')
+pancom.pack(side=LEFT)
+
+# Drinks panel
+pancom = LabelFrame(panizq,text='Drinks',font=('Dosis',19,'bold'),
+                    bd=1, relief=FLAT, fg='azure4')
+pancom.pack(side=LEFT)
+
+# Desert panel
+pancom = LabelFrame(panizq,text='Desert',font=('Dosis',19,'bold'),
+                    bd=1, relief=FLAT, fg='azure4')
+pancom.pack(side=LEFT)
+
+# right panel
+pander =Frame(app,bg=1,relief=FLAT)
+pander.pack(side=RIGHT)
+
+# calculator panel
+pancal = Frame(pander,bd=1,relief=FLAT, bg='burlywood')
+pancal.pack()
+
+# ticket panel
+pantic = Frame(pander,bd=1,relief=FLAT, bg='burlywood')
+pantic.pack()
+
+# buttons panel
+panbot = Frame(pander,bd=1,relief=FLAT, bg='burlywood')s
+panbot.pack()
 
 app.mainloop()
 
