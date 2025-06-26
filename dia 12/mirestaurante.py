3f072625ddf46fad11949c8986946791c33c49cc
Juan Pablo Mayagoitia Franco <103715117+jpmfranco@users.noreply.github.com>
2025-06-26 14:50:45 -0600

    day 12 start

---
+++ dia 12/mirestaurante.py
@@ -0,0 +1,23 @@
+from tkinter import *
+
+#iniciar tkinter
+
+app = Tk()
+
+
+# size of window
+app.geometry('1020x630+0+0')
+
+#avoid max
+app.resizable(0,0)
+
+# title of the window
+app.title("My restaurant - Facturation system")
+
+# background color
+app.config(bg='burlywood')
+
+# Avoid that the screen close
+
+app.mainloop()
+
